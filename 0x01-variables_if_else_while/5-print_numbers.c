34c4c8fcc50a8bc5add24b76b059533db324a76e
chima <mdonald489@gmail.com>
2022-09-12 13:06:53 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,42 @@
-#include <stdio.h>                                                                                                                                                         
-/**                                                                                                                             
- * main - print if the number is positive, zero, or negative                                                                    
- *                                                                                                                              
- * Description: using the main function                                                                                         
- * this program prints Programming is positive, zero, or negative                                                              
- * Return: 0                                                                                                                    
- */                                                                                                                             
-int main(void)                                                                                                                  
-{                                                                                                                               
-        int i;                                                                                                                  
-        for (i = 0; i < 10; i++)                                                                                                
-        {                                                                                                                       
-                printf("%i", i);                                                                                                
-        }                                                                                                                       
-        printf("\n");                                                                                                           
-        return (0);                                                                                                             
+#include <stdio.h>
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+
+
+{
+
+int i; /*Decarling statement*/
+
+
+
+/*for count 0-9*/
+
+for (i = 0 ; i < 10; i++)
+
+{
+
+printf("%i", i); /*print i*/
+
 }
+
+
+
+printf("\n"); /*new line*/
+
+
+
+return (0);
+
+
+
