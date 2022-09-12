022566599528b54bce153147538d96a0e19d28ee
chima <mdonald489@gmail.com>
2022-09-12 13:14:06 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 /**
-
  * main - Entry point
-
- *
 
  * Return: Always 0 (Success)
 
@@ -12,31 +9,19 @@
 
 int main(void)
 
-
-
 {
 
 int i; /*Decarling statement*/
-
-
 
 /*for count 0-9*/
 
 for (i = 0 ; i < 10; i++)
 
 {
-
 printf("%i", i); /*print i*/
 
 }
-
-
-
 printf("\n"); /*new line*/
-
-
-
 return (0);
-
-
+}
 
