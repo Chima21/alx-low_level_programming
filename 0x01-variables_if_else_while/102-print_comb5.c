191176d030eea4f295c6de5158c1e087ab451851
chima <mdonald489@gmail.com>
2022-09-12 17:34:25 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,11 +6,9 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
 	int first_nums; /*Decarling statement*/
 	int second_nums;
-
 	/*for 0-9 with ASCII*/
 	for (first_nums = 0; first_nums < 100 ; first_nums++)
 	{
@@ -18,22 +16,20 @@ int main(void)
 		{
 			putchar (first_nums / 10 + '0'); /*print  first nums, first digit*/
 			putchar (first_nums % 10 + '0'); /*print  first nums, second digit*/
-			
 			putchar (' '); /*prirint a space*/
 			putchar (second_nums / 10 + '0'); /*print  sencond nums, first digit*/
 			putchar (second_nums % 10 + '0'); /*print  second nums, second digit*/
-			/*break program if the condition is true (if is 98 99)*/if (first_nums == 98 && second_nums == 99)
+			/*break program if the condition is true (if is 98 99)*/
+			if (first_nums == 98 && second_nums == 99)
 			{
 				break;
 			}
 			putchar(','); /*print (,)*/
+			putchar (' '); /*prirint a space*/
+		} /*end for #2*/
 
-			putchar (' '); /*print a space*/
-
-		        } /*end for #2*/	
 	} /*end for #1*/
 	putchar('\n'); /*new line*/
 	return (0);
 }
 
-				
